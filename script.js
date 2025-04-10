547200e0032df56669cd52d36b142e323be97943
Jeffrin Samuel <jeffrinsamuel2006@gmail.comc:\Users\admin\OneDrive\Pictures\Screenshots\Javascript\.git\COMMIT_EDITMSG>
2025-04-10 21:13:04 +0530

    Clean push: Updated banner and responsiveness

---
+++ script.js
@@ -0,0 +1,165 @@
+<<<<<<< HEAD
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Passionate Developer", "Leetcoder"],
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Future Developer", "Leetcoder"],
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // stops the form from actually submitting
+    alert("Thanks for reaching out! This form is just a demo");
+    form.reset(); // clears the form
+  });
+=======
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
+
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Passionate Developer", "Leetcoder"],
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Student", "Future Developer", "Leetcoder"],
+        typeSpeed: 70,
+        backSpeed: 50,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
+
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault(); // stops the form from actually submitting
+    alert("Thanks for reaching out! This form is just a demo");
+    form.reset(); // clears the form
+  });
+>>>>>>> 4d97089 (Updated banner responsiveness and final changes)
